41dea52893c6c6763ae7a64cc74af8fb43ca97d9
mfesilva <87022946+mfesilva@users.noreply.github.com>
2021-07-07 16:07:16 +0100

    Update Transfer_MBwayController.java

--- sise-bank/src/main/java/pt/ulisboa/tecnico/learnjava/mbway/controllers/Transfer_MBwayController.java
+++ sise-bank/src/main/java/pt/ulisboa/tecnico/learnjava/mbway/controllers/Transfer_MBwayController.java
@@ -1,21 +1,18 @@
 package pt.ulisboa.tecnico.learnjava.mbway.controllers;
 
+import pt.ulisboa.tecnico.learnjava.bank.domain.TransferData;
 import pt.ulisboa.tecnico.learnjava.bank.exceptions.AccountException;
 import pt.ulisboa.tecnico.learnjava.bank.services.Services;
 import pt.ulisboa.tecnico.learnjava.mbway.domain.MBway;
 
 public class Transfer_MBwayController {
-
+	
 	private MBway clients;
-	private String source_phone;
-	private String target_phone;
-	private Integer amount;
+	private TransferData data;
 	private Services services = new Services();
 	
-	public Transfer_MBwayController(String source_phone, String target_phone, Integer amount, MBway clients) {
-		this.source_phone = source_phone;
-		this.target_phone = target_phone;
-		this.amount = amount;
+	public Transfer_MBwayController(TransferData data, MBway clients) {
+		this.data = data;
 		this.clients = clients;
 	}
 	
